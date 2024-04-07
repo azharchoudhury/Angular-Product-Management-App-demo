f3e68041fa2d071d1fc2b64895c9ff87d181b453
azharchoudhury <azharmehmud11@gmail.com>
2024-04-08 00:55:28 +0530

    Initial Product Card created

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ProductCardComponent } from '../product-card/product-card.component';
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ProductCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
