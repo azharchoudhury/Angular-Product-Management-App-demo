872415ad9d6530e43d383fc04819c88a0a2f729d
azharchoudhury <azharmehmud11@gmail.com>
2024-04-10 01:29:44 +0530

    component communication

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,14 +1,84 @@
 import { Component } from '@angular/core';
 import { ProductCardComponent } from '../product-card/product-card.component';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProductCardComponent],
+  imports: [ProductCardComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  products = [
+    {
+      "id": "1",
+      "brand": "PUMA",
+      "image": "https://rukminim2.flixcart.com/image/612/612/xif0q/t-shirt/h/d/g/3xl-78004501-puma-original-imagy5wnnanhgrsy.jpeg?q=70",
+      "currentPrice": "6649",
+      "standardPrice": "7999",
+      "discount": "16",
+      "name": "Men Printed, Typography V Neck Polyester Blue T-Shirt"
+    },
+    {
+      "id": "2",
+      "brand": "Louis Philippe",
+      "image": "https://rukminim2.flixcart.com/image/612/612/xif0q/t-shirt/h/q/w/3xl-lxkpargfm00595-louis-philippe-original-imagg5a8rztyymbt.jpeg?q=70",
+      "currentPrice": "4849",
+      "standardPrice": "4999",
+      "discount": "3",
+      "name": "Men Solid Polo Neck Pure Cotton Purple T-Shirt"
+    },
+    {
+      "id": "3",
+      "brand": "Tommy Hilfiger",
+      "image": "https://rukminim2.flixcart.com/image/612/612/xif0q/t-shirt/m/v/4/-original-imagyjazxhzm46uv.jpeg?q=70",
+      "currentPrice": "3499",
+      "standardPrice": "4999",
+      "discount": "30",
+      "name": "Men Solid Polo Neck Cotton Blend Orange T-Shirt"
+    },
+    {
+      "id": "4",
+      "brand": "Adidas",
+      "image": "https://rukminim2.flixcart.com/image/612/612/xif0q/t-shirt/x/9/d/-original-imaguy6jbhhracde.jpeg?q=70",
+      "currentPrice": "4133",
+      "standardPrice": "5299",
+      "discount": "22",
+      "name": "Men Printed V Neck Polyester Green T-Shirt"
+    },
+    {
+      "id": "5",
+      "brand": "U.S. Polo Assn",
+      "image": "https://rukminim2.flixcart.com/image/612/612/xif0q/t-shirt/w/u/o/s-ustshp0208-u-s-polo-assn-original-imagvbtvjhredvhs.jpeg?q=70",
+      "currentPrice": "3349",
+      "standardPrice": "3499",
+      "discount": "4",
+      "name": "Men Solid Polo Neck Pure Cotton Green T-Shirt"
+    },
+    {
+      "id": "6",
+      "brand": "Louis Philippe",
+      "image":   "https://rukminim2.flixcart.com/image/612/612/xif0q/t-shirt/j/x/7/xs-lxkpargfw71880-louis-philippe-original-imagg5a8g3dgj3y6.jpeg?q=70"
+      ,
+      "currentPrice": "3449",
+      "standardPrice": "4999",
+      "discount": "31",
+      "name": "Men Solid Polo Neck Pure Cotton White T-Shirt"
+    },
+    {
+      "id": "7",
+      "brand": "Adidas",
+      "image":   "https://rukminim2.flixcart.com/image/612/612/xif0q/t-shirt/1/f/b/m-he2981-adidas-original-imagg39akzkpjzpr.jpeg?q=70"
+      ,
+      "currentPrice": "3199",
+      "standardPrice": "4999",
+      "discount": "36",
+      "name": "MUFC Men Printed Round Neck Polyester Green T-Shirt"
+    },
 
+
+
+  ]
 }
--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
-import {MatCardModule} from '@angular/material/card';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatButtonModule, CommonModule],
   templateUrl: './product-card.component.html',
-  styleUrl: './product-card.component.scss'
+  styleUrl: './product-card.component.scss',
 })
-export class ProductCardComponent {
 
+export class ProductCardComponent {
+  @Input() product: any;
+
+  view(){
+    console.log("View clicked")
+  }
 }
